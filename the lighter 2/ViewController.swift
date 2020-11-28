8b44e0849eb236979b3633e91e7f28e96101e704
Vladimir Belyakov <belyakov_vova@mail.ru>
2020-11-28 12:50:57 +0300

    Initial Commit

---
+++ the lighter 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  the lighter 2
+//
+//  Created by VB on 28.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
